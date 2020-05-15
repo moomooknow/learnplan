f4d02a85f30f0639d043f18f7e21bd7242c31763
moomooknow <1521629305@qq.com>
2020-05-15 18:35:35 +0800

    volatile 使用

---
+++ learnplan/src/main/java/com/example/learnplan/thread/VolatileTest.java
@@ -0,0 +1,38 @@
+package com.example.learnplan.thread;
+
+/**
+ * @author wangfk
+ */
+public class VolatileTest {
+    public static void main(String[] args) {
+        ThreadRun threadRun = new ThreadRun();
+        threadRun.start();
+        // main 线程等待 threadRun执行完毕    
+        // threadRun.join();
+        for(;;) {
+            synchronized (threadRun){
+                if(threadRun.isFlag()) {
+                    System.out.println("你变了");
+                }
+            }
+        }
+    }
+}
+class ThreadRun extends Thread{
+    private volatile boolean flag = false;
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        flag = true;
+        System.out.println("flag = " + flag);
+    }
+}
