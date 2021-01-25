37a9a81bf9babbf6d361cc8f58675e4e0cbb038e
moomoo <moomooknow@163.com>
2021-01-25 20:30:45 +0800

    (*)

---
+++ learnplan/src/main/java/com/example/learnplan/hutool/TestStr.java
@@ -0,0 +1,11 @@
+package com.example.learnplan.hutool;
+
+import java.io.File;
+
+/** @author wangfk */
+public class TestStr {
+  public static void main(String[] args) {
+    String sysPath = new File(TestStr.class.getResource("").getPath()).getPath() + "/temp/";
+    System.out.println(sysPath);
+  }
+}
